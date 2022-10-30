85a2c394607f029c04e0abe0cf74c51b392fa534
Karan Venkatesh Upamanyu <karan.v.upamanyu@gmail.com>
2022-10-30 17:24:48 +0530

    this is a speedrun

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+app.listen(3000, () => console.log(`Hello world app listening on port 3000!`));
